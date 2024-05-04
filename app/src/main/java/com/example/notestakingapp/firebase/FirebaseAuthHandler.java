dbaba12ebe3b65bc0c6e4533c2e2f0b0d993cad2
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-05-05 00:21:42 +0700

    tao file Tong

---
+++ app/src/main/java/com/example/notestakingapp/firebase/FirebaseAuthHandler.java
@@ -0,0 +1,10 @@
+package com.example.notestakingapp.firebase;
+
+public class FirebaseAuthHandler {
+    public void signIn() {}
+    public void signUp() {}
+    public void signOut() {}
+    public void getUserId() {} //todo: localId = -1
+    public void getUserEmail() {}
+
+}
