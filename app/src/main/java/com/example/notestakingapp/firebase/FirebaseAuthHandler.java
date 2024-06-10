535304a36f9486f5c53d0d27268387eabbcd9131
Le Nhat Hoang <lenhathoang2004@gmail.com>
2024-06-10 21:30:46 +0700

    chore: minor changes

--- app/src/main/java/com/example/notestakingapp/firebase/FirebaseAuthHandler.java
+++ app/src/main/java/com/example/notestakingapp/firebase/FirebaseAuthHandler.java
@@ -54,45 +54,45 @@ public class FirebaseAuthHandler {
 
     public void signUp(String email, String password, final Context context) {
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user, context);
-                        } else {
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(context, "Sign Up failed: " + task.getException().getMessage(),
-                                    Toast.LENGTH_LONG).show();
-                            updateUI(null, context);
-                        }
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "createUserWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user, context);
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        Toast.makeText(context, "Sign Up failed: " + task.getException().getMessage(),
+                                Toast.LENGTH_LONG).show();
+                        updateUI(null, context);
                     }
-                });
+                }
+            });
     }
     public void signIn(String email, String password, final Context context) {
         mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            SharedPreferences sharedPref = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedPref.edit();
-                            editor.putString("userEmail", email);
-                            editor.apply();
-                            updateUI(user, context);
-                            FirebaseHandler.syncFromFirebase(context);
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        SharedPreferences sharedPref = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString("userEmail", email);
+                        editor.apply();
+                        updateUI(user, context);
+                        FirebaseHandler.syncFromFirebase(context);
 
-                        } else {
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(context, "Sign In failed: " + task.getException().getMessage(),
-                                    Toast.LENGTH_LONG).show();
-                            updateUI(null, context);
-                        }
+                    } else {
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(context, "Sign In failed: " + task.getException().getMessage(),
+                                Toast.LENGTH_LONG).show();
+                        updateUI(null, context);
                     }
-                });
+                }
+            });
     }
     public void signOut(Context context) {
         String userId = FirebaseAuthHandler.getUserId();
@@ -125,9 +125,9 @@ public class FirebaseAuthHandler {
 
     private void configureGoogleSignIn(Context context) {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(context.getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
+            .requestIdToken(context.getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
     }
@@ -175,29 +175,29 @@ public class FirebaseAuthHandler {
         FirebaseUser user = mAuth.getCurrentUser();
         if (user != null) {
             mAuth.signInWithEmailAndPassword(Objects.requireNonNull(user.getEmail()), oldPassword)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                FirebaseUser currentUser = mAuth.getCurrentUser();
-                                if (currentUser != null) {
-                                    currentUser.updatePassword(newPassword)
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                @Override
-                                                public void onComplete(@NonNull Task<Void> task) {
-                                                    if (task.isSuccessful()) {
-                                                        Toast.makeText(context, "Password updated successfully", Toast.LENGTH_SHORT).show();
-                                                    } else {
-                                                        Toast.makeText(context, "Failed to update password: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                                                    }
-                                                }
-                                            });
-                                }
-                            } else {
-                                Toast.makeText(context, "Authentication failed: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser currentUser = mAuth.getCurrentUser();
+                            if (currentUser != null) {
+                                currentUser.updatePassword(newPassword)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(context, "Password updated successfully", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Toast.makeText(context, "Failed to update password: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
                             }
+                        } else {
+                            Toast.makeText(context, "Authentication failed: " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                         }
-                    });
+                    }
+                });
         } else {
             Toast.makeText(context, "User not signed in", Toast.LENGTH_SHORT).show();
         }
