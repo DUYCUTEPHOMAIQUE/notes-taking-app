e83acabc4930e6c9be63f507f2014f835fc4e848
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2025-11-03 23:34:12 +0700

    fix something up to gg play

---
+++ app/src/main/java/com/supernote/app/database/NoteComponent/Note.java
@@ -0,0 +1,48 @@
+package com.supernote.app.database.NoteComponent;
+
+public class Note {
+    private int noteId;
+    private String title;
+    private long createAt;
+    private String color;
+
+    public int getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(int noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(long createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public Note(int noteId, String title, long createAt, String color) {
+        this.noteId = noteId;
+        this.title = title;
+        this.createAt = createAt;
+        this.color = color;
+    }
+}
---
+++ app/src/main/java/com/supernote/app/ui/ViewImageActivity.java
@@ -0,0 +1,42 @@
+package com.supernote.app.ui;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.supernote.app.R;
+import com.supernote.app.database.DatabaseHandler;
+import com.supernote.app.utils.ImageUtils;
+
+public class ViewImageActivity extends AppCompatActivity {
+    int imageId;
+    ImageView imageView;
+    TextView backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_view_image);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        imageView = findViewById(R.id.image_view);
+        backButton = findViewById(R.id.back_button_view_full);
+        backButton.setOnClickListener(v -> {
+            getOnBackPressedDispatcher().onBackPressed();
+        });
+        imageId = getIntent().getIntExtra("image_id", -1);
+        if (imageId != -1) {
+            imageView.setImageBitmap(ImageUtils.byteToBitmap(DatabaseHandler.getImageById(this, imageId)));
+        }
+    }
+}
